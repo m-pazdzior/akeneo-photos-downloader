3e7075bc7aa14a77ff64122e5985b2ab458c4017
Maciej Paździor <m.pazdzior@fca.com.pl>
2024-02-19 11:21:26 +0100

    Zapisywanie zdjęć w archwium zip

--- photos_downloader.py
+++ photos_downloader.py
@@ -2,16 +2,20 @@ import csv
 import requests 
 from PIL import Image 
 import urllib3
+import zipfile
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-with open('photo_script_test.csv') as photos_csv:
-    photos_csv = list(csv.reader(photos_csv, delimiter = ';'))
-    for row in photos_csv[1:]:
-        url = row[1]
-        if url:
-            data = requests.get(url, verify=False).content
-            file_name = f'{row[0]}.jpg'
-            f = open(file_name, 'wb')
-            f.write(data)
-            f.close()
+with zipfile.ZipFile("photos.zip", mode="w") as archive:
+    with open('photo_script_test.csv') as photos_csv:
+        photos_csv = list(csv.reader(photos_csv, delimiter = ';'))
+        filenames = []
+
+        for row in photos_csv[1:]:
+            url = row[1]
+            if url:
+                data = requests.get(url, verify=False).content
+                file_name = f'{row[0]}.jpg'
+                filenames.append(file_name)
+                archive.writestr(file_name, data)
+    
