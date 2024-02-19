2d6cc0fae7e8a2758829327b7c33816167fee1e2
Maciej Paździor <m.pazdzior@fca.com.pl>
2024-02-19 14:20:40 +0100

    Modyfikacja csv

--- photos_downloader.py
+++ photos_downloader.py
@@ -6,16 +6,22 @@ import zipfile
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-with zipfile.ZipFile("photos.zip", mode="w") as archive:
-    with open('photo_script_test.csv') as photos_csv:
-        photos_csv = list(csv.reader(photos_csv, delimiter = ';'))
-        filenames = []
+with zipfile.ZipFile("photos.zip", mode="w") as photos_zip:
+    with open('photo_script_test.csv','r') as photos_csv:
+        photos_csv_reader = list(csv.reader(photos_csv, delimiter = ';'))
+        lines_to_update = {}
 
-        for row in photos_csv[1:]:
+        for line, row in enumerate(photos_csv_reader[1:]):
             url = row[1]
             if url:
                 data = requests.get(url, verify=False).content
                 file_name = f'{row[0]}.jpg'
-                filenames.append(file_name)
-                archive.writestr(file_name, data)
+                photos_zip.writestr(file_name, data)
+                lines_to_update[line+2] = [row[0], f'/{file_name}']
     
+    with open('photo_script_test.csv', 'w', newline='') as photos_csv:
+        photos_csv_writer = csv.writer(photos_csv)
+
+        for line, row in enumerate(photos_csv_reader):
+            data = lines_to_update.get(line+1, row)
+            photos_csv_writer.writerow(data)
