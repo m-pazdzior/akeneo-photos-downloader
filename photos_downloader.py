06830547c69c2e2f4da1227ed5c6933c26a5c11f
Maciej Paździor <m.pazdzior@fca.com.pl>
2024-02-19 15:06:11 +0100

    Zmiana sposobu tworzenia csv

--- photos_downloader.py
+++ photos_downloader.py
@@ -9,19 +9,21 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 with zipfile.ZipFile("photos.zip", mode="w") as photos_zip:
     with open('photo_script_test.csv','r') as photos_csv:
         photos_csv_reader = list(csv.reader(photos_csv, delimiter = ';'))
-        lines_to_update = {}
-
-        for line, row in enumerate(photos_csv_reader[1:]):
-            url = row[1]
-            if url:
-                data = requests.get(url, verify=False).content
-                file_name = f'{row[0]}.jpg'
-                photos_zip.writestr(file_name, data)
-                lines_to_update[line+2] = [row[0], f'/{file_name}']
-    
-    with open('photo_script_test.csv', 'w', newline='') as photos_csv:
-        photos_csv_writer = csv.writer(photos_csv)
+        rows = []
 
         for line, row in enumerate(photos_csv_reader):
-            data = lines_to_update.get(line+1, row)
-            photos_csv_writer.writerow(data)
+            if line >= 1:
+                url = row[1]
+                if url:
+                    data = requests.get(url, verify=False).content
+                    file_name = f'{row[0]}.jpg'
+                    photos_zip.writestr(file_name, data)
+                    rows.append([row[0], f'/{file_name}'])
+            else:
+                fields = row
+
+    with open('import_photos.csv', 'w', newline='') as photos_csv:
+        photos_csv_writer = csv.writer(photos_csv, delimiter=';')
+        photos_csv_writer.writerow(fields)
+        photos_csv_writer.writerows(rows)
+
